1de52f85280f723b4d242bc79fd9db5e7178cc8a
mxp <maxplayer@free.fr>
2023-05-01 23:45:20 +0200

    add 30 days for the days starting the refactoring

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,35 +5,49 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def increase_quality(self, item):
+        item.quality = item.quality + 1
+        return item.quality
+
+    def decrease_quality(self, item):
+        item.quality = item.quality - 1
+        return item.quality
+   
+    def decrease_quality_whenever_quality_is_positive(self, item):
+        if item.quality > 0:
+            item.quality = self.decrease_quality(item)
+        return item.quality
+
+
+    def increase_quality_whenever_quality_below_50(self, item):
+        if item.quality < 50:
+            item.quality = self.increase_quality(item)
+        return item.quality
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = self.decrease_quality_whenever_quality_is_positive(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                item.quality = self.increase_quality_whenever_quality_below_50(item)
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 11:
+                    item.quality = self.increase_quality_whenever_quality_below_50(item)
+                if item.sell_in < 6:
+                    item.quality = self.increase_quality_whenever_quality_below_50(item)
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                if item.name == "Aged Brie":
+                    item.quality = self.increase_quality_whenever_quality_below_50(item)
+                else :
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        item.quality = 0
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = self.decrease_quality_whenever_quality_is_positive(item)
 
 
 class Item:
