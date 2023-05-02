bf8bb0725872b4652b778a1bf2dc4bce93c41b9a
mxp <maxplayer@free.fr>
2023-05-02 23:56:03 +0200

    add debug inspector update the refactoring

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,49 +6,45 @@ class GildedRose(object):
         self.items = items
 
     def increase_quality(self, item):
-        item.quality = item.quality + 1
-        return item.quality
+        if item.quality < 50:
+            item.quality = item.quality + 1
 
     def decrease_quality(self, item):
-        item.quality = item.quality - 1
-        return item.quality
-   
-    def decrease_quality_whenever_quality_is_positive(self, item):
         if item.quality > 0:
-            item.quality = self.decrease_quality(item)
-        return item.quality
+            item.quality = item.quality - 1
 
-
-    def increase_quality_whenever_quality_below_50(self, item):
-        if item.quality < 50:
-            item.quality = self.increase_quality(item)
-        return item.quality
+    def decrease_sell_in(self, item):
+        item.sell_in = item.sell_in - 1
 
     def update_quality(self):
+
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
-                item.quality = self.decrease_quality_whenever_quality_is_positive(item)
-            else:
-                item.quality = self.increase_quality_whenever_quality_below_50(item)
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.increase_quality(item)
                 if item.sell_in < 11:
-                    item.quality = self.increase_quality_whenever_quality_below_50(item)
+                    self.increase_quality(item)
                 if item.sell_in < 6:
-                    item.quality = self.increase_quality_whenever_quality_below_50(item)
+                    self.increase_quality(item)
+                self.decrease_sell_in(item)
+                if item.sell_in < 0:
+                    item.quality = 0
+                continue
 
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            if item.name == "Aged Brie": 
+                self.increase_quality(item)
+                self.decrease_sell_in(item)
+                if item.sell_in < 0:
+                    self.increase_quality(item)
+                continue
 
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
+            self.decrease_quality(item)
+            self.decrease_sell_in(item)
             if item.sell_in < 0:
-                if item.name == "Aged Brie":
-                    item.quality = self.increase_quality_whenever_quality_below_50(item)
-                else :
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = 0
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = self.decrease_quality_whenever_quality_is_positive(item)
-
+                self.decrease_quality(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -58,3 +54,39 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class Sellable():
+    def __init__(self, item):
+        self.name = item.name
+        self.sell_in = item.sell_in
+        self.quality = item.quality
+
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+if __name__ == "__main__":
+    print ("OMGHAI!")
+    items = [
+             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+            ]
+
+    days = 31
+    import sys
+    if len(sys.argv) > 1:
+        days = int(sys.argv[1]) + 1
+    for day in range(days):
+        print("-------- day %s --------" % day)
+        print("name, sellIn, quality")
+        for item in items:
+            print(item)
+        print("")
+        GildedRose(items).update_quality()
