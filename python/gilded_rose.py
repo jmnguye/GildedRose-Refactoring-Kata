b57f3c65dbfbe0bf30b6f78060564a898e71727d
mxp <maxplayer@free.fr>
2023-05-03 23:07:55 +0200

    refactoring done, change if condition to use instance instead, but had to also change how item are instanciate in tests.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,52 +1,18 @@
 # -*- coding: utf-8 -*-
 
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def increase_quality(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-
-    def decrease_quality(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-
-    def decrease_sell_in(self, item):
-        item.sell_in = item.sell_in - 1
-
     def update_quality(self):
-
         for item in self.items:
+            item.update_quality()
 
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self.increase_quality(item)
-                if item.sell_in < 11:
-                    self.increase_quality(item)
-                if item.sell_in < 6:
-                    self.increase_quality(item)
-                self.decrease_sell_in(item)
-                if item.sell_in < 0:
-                    item.quality = 0
-                continue
-
-            if item.name == "Aged Brie": 
-                self.increase_quality(item)
-                self.decrease_sell_in(item)
-                if item.sell_in < 0:
-                    self.increase_quality(item)
-                continue
-
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                continue
-
-            self.decrease_quality(item)
-            self.decrease_sell_in(item)
-            if item.sell_in < 0:
-                self.decrease_quality(item)
 
 class Item:
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -55,38 +21,62 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-class Sellable():
-    def __init__(self, item):
-        self.name = item.name
-        self.sell_in = item.sell_in
-        self.quality = item.quality
+
+class Sellable(Item):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def increase_quality(self):
+        if self.quality < 50:
+            self.quality = self.quality + 1
+
+    def decrease_quality(self):
+        if self.quality > 0:
+            self.quality = self.quality - 1
+
+    def decrease_sell_in(self):
+        self.sell_in = self.sell_in - 1
+
+    def update_quality(self):
+        self.decrease_quality()
+        self.decrease_sell_in()
+        if self.sell_in < 0:
+            self.decrease_quality()
 
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+class AgedBrie(Sellable):
 
-if __name__ == "__main__":
-    print ("OMGHAI!")
-    items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
-    days = 31
-    import sys
-    if len(sys.argv) > 1:
-        days = int(sys.argv[1]) + 1
-    for day in range(days):
-        print("-------- day %s --------" % day)
-        print("name, sellIn, quality")
-        for item in items:
-            print(item)
-        print("")
-        GildedRose(items).update_quality()
+    def update_quality(self):
+            self.increase_quality()
+            self.decrease_sell_in()
+            if self.sell_in < 0:
+                self.increase_quality()
+
+
+class Sulfuras(Sellable):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        pass
+
+
+class BackstagePasses(Sellable):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+            self.increase_quality()
+            if self.sell_in < 11:
+                self.increase_quality()
+            if self.sell_in < 6:
+                self.increase_quality()
+            self.decrease_sell_in()
+            if self.sell_in < 0:
+                self.quality = 0
